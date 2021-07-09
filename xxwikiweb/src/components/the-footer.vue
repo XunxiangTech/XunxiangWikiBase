f94cb63529a1d3818af6a83237c1f84c54301ab1
Oliver Chen <56765896+YushunChen@users.noreply.github.com>
2021-07-09 19:38:12 +0800

    Setup(Homepage): Header and footer components

---
+++ xxwikiweb/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    寻向智库 ©2021 Created by 寻向智库Tech
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
