77afb7603365b3435a729b1a05a02cdc531fc14a
YingzeHou <yhou63@wisc.edu>
2021-07-23 17:03:21 +0800

    1.1.9-Cloud Redis Config & Revert Shiro to no auth

--- src/main/java/com/xunxiang/xunxiangwikibase/controller/WikibookController.java
+++ src/main/java/com/xunxiang/xunxiangwikibase/controller/WikibookController.java
@@ -27,7 +27,7 @@ public class WikibookController {
     @Autowired
     WikibookServiceImpl wikibookService;
 
-    @RequiresPermissions("userinfo:view")
+    //@RequiresPermissions("userinfo:view")
     @GetMapping("/list")
     public CommonResp list(@Valid WikibookQueryReq wikibookQueryReq){
         CommonResp<PageResp<WikibookResp>> commonResp = new CommonResp<>();
